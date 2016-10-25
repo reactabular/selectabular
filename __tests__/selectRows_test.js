7ffce2fcfc1b6b85b4d7eb0bfb6f169f6d3e9e0a
toni <unmail.toni@gmail.com>
2016-10-25 23:27:37 +0200

    All and none test

--- __tests__/selectRows_test.js
+++ __tests__/selectRows_test.js
@@ -1,5 +1,6 @@
 /* eslint-disable */
-import selectabular from '../dist/selectabular';
+//import selectabular from '../dist/selectabular';
+import selectabular from '../src/index';
 
 const can1 = {
   flavor: 'grapefruit',
@@ -9,13 +10,23 @@ const can2 = {
   flavor: 'grapefruit',
   ounces: 12,
 };
+const myrows = [
+  { id: 10, product: 'apple', company: 'Apple Inc', price: 1.5, stock: 300 },
+  { id: 11, product: 'pear', company: 'Pear Inc', price: 3, stock: 1000 },
+  { id: 12, product: 'grape', company: 'Grapesoft', price: 22.1, stock: 18 },
+  { id: 13, product: 'banana', company: 'Banana Tech', price: 12, stock: 9 }
+];
 
-describe('the La Croix cans on my desk', () => {
-  it('have all the same properties', () => {
-    expect(can1).toEqual(can2);
-  });
-  it('are not the exact same can', () => {
-    expect(can1).not.toBe(can2);
+describe('select.all', () => {
+  const allSelected = selectabular.all(myrows);
+  const expected = [
+    { id: 10, selected:true, product: 'apple', company: 'Apple Inc', price: 1.5, stock: 300 },
+    { id: 11, selected:true, product: 'pear', company: 'Pear Inc', price: 3, stock: 1000 },
+    { id: 12, selected:true, product: 'grape', company: 'Grapesoft', price: 22.1, stock: 18 },
+    { id: 13, selected:true, product: 'banana', company: 'Banana Tech', price: 12, stock: 9 }
+  ];
+  it('returns all rows with selected=true', () => {
+    expect(allSelected).toEqual(expected);
   });
 });
 
--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-const greeting = 'aloha';
+const select = {
+  all: function(rows){
+    return rows.map(r => ({...r, selected:true}) )
+  },
+  none: function(rows){
+    return rows.map(r => ({...r, selected:false}) )
+  }
+};
 
-export default greeting;
+export default select;
