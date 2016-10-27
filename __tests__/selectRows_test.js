551c01d6c58fb82123f1e2ff7aefc046f71756d6
toni <unmail.toni@gmail.com>
2016-10-28 00:49:07 +0200

    All API covered, alternative could be all based on 'actOnFiltered'

--- __tests__/selectRows_test.js
+++ __tests__/selectRows_test.js
@@ -43,33 +43,65 @@ describe('select.none', () => {
     { id: 13, selected:false, product: 'banana', company: 'Banana Tech', price: 12, stock: 9 }
   ];
   const result = selectabular.none(initRows);
-  it('returns all rows with selected=true', () => {
+  it('returns all rows with selected=false', () => {
     expect(result).toEqual(expected);
   });
 });
 
-/*
-const myrows = [
-  { id: 10, product: 'apple', company: 'Apple Inc', price: 1.5, stock: 300 },
-  { id: 11, product: 'pear', company: 'Pear Inc', price: 3, stock: 1000 },
-  { id: 12, product: 'grape', company: 'Grapesoft', price: 22.1, stock: 18 },
-  { id: 13, product: 'banana', company: 'Banana Tech', price: 12, stock: 9 }
-];
-
-describe('select.selectRow', function () {
-  it('returns matching row by Id', function () {
-    const myId = 12;
-    const expecSel = [
-      { id: 12, product: 'grape', company: 'Grapesoft', price: 22.1, stock: 18 }
-    ];
-    const result = selectRow({ rows: myrows, selectedRowId: myId });
-    expect(result.selectedRows).to.deep.eq(expecSel);
-  });
-  it('returns nothing when not matching', function () {
-    const myId = 1992;
-    const expecSel = [];
-    const result = selectRow({ rows: myrows, selectedRowId: myId });
-    expect(result.selectedRows).to.deep.eq(expecSel);
+describe('select.nonefree', () => {
+  const initRows = [
+    { id: 10, product: 'apple', company: 'Apple Inc', price: 1.5, stock: 300 },
+    { id: 11, selected:true, product: 'pear', company: 'Pear Inc', price: 3, stock: 1000 },
+    { id: 12, product: 'grape', company: 'Grapesoft', price: 22.1, stock: 18 },
+    { id: 13, selected:false, product: 'banana', company: 'Banana Tech', price: 12, stock: 9 }
+  ];
+  const expected = [
+    { id: 10,  product: 'apple', company: 'Apple Inc', price: 1.5, stock: 300 },
+    { id: 11,  product: 'pear', company: 'Pear Inc', price: 3, stock: 1000 },
+    { id: 12,  product: 'grape', company: 'Grapesoft', price: 22.1, stock: 18 },
+    { id: 13,  product: 'banana', company: 'Banana Tech', price: 12, stock: 9 }
+  ];
+  const result = selectabular.nonefree(initRows);
+  it('returns all rows with no selected field', () => {
+    expect(result).toEqual(expected);
   });
 });
-*/
+describe('select.rows', () => {
+  const initRows = [
+    { id: 10, product: 'apple', company: 'Apple Inc', price: 1.5, stock: 300 },
+    { id: 11,  product: 'pear', company: 'Pear Inc', price: 3, stock: 1000 },
+    { id: 12, product: 'grape', company: 'Grapesoft', price: 22.1, stock: 18 },
+    { id: 13, product: 'banana', company: 'Banana Tech', price: 12, stock: 9 }
+  ];
+  const fil = row => row.price > 5
+  const expected = [
+    { id: 12,  product: 'grape', company: 'Grapesoft', price: 22.1, stock: 18 },
+    { id: 13,  product: 'banana', company: 'Banana Tech', price: 12, stock: 9 }
+  ];
+  const {rows, selectedRows: result } = selectabular.rows(fil)(initRows);
+  it('expect correct filtering of rows', () => {
+    expect(result).toEqual(expected);
+  });
+});
+describe('select.toggle', () => {
+  const initRows = [
+    { id: 10, selected: false, product: 'apple', company: 'Apple Inc', price: 1.5, stock: 300 },
+    { id: 11, selected: true, product: 'pear', company: 'Pear Inc', price: 3, stock: 1000 },
+    { id: 12, product: 'grape', company: 'Grapesoft', price: 22.1, stock: 18 },
+    { id: 13, product: 'banana', company: 'Banana Tech', price: 12, stock: 9 }
+  ];
+  const fil = row => row.id < 12
+  //Returns matched ones inverting 'selected'
+  const expected = [
+    { id: 10, selected: true, product: 'apple', company: 'Apple Inc', price: 1.5, stock: 300 },
+    { id: 11, selected: false, product: 'pear', company: 'Pear Inc', price: 3, stock: 1000 }
+  ];
+  const result = selectabular.toggle(fil)(initRows);
+  it('expect correct filering of rows', () => {
+    expect(result).toEqual(expected);
+  });
+});
+
+
+
+
