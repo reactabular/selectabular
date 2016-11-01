242a22a0613927f69d1841b671d92fb82f9194c6
toni <unmail.toni@gmail.com>
2016-11-01 18:52:03 +0100

    Re-implemented functions, removed nonefree, fixed 'rows', added contributors

--- __tests__/selectRows_test.js
+++ __tests__/selectRows_test.js
@@ -1,14 +1,6 @@
 /* eslint-disable */
-const selectabular = require('../src').default;
+import selectabular from '../src/index';
 
-const can1 = {
-  flavor: 'grapefruit',
-  ounces: 12,
-};
-const can2 = {
-  flavor: 'grapefruit',
-  ounces: 12,
-};
 const myrows = [
   { id: 10, product: 'apple', company: 'Apple Inc', price: 1.5, stock: 300 },
   { id: 11, product: 'pear', company: 'Pear Inc', price: 3, stock: 1000 },
@@ -46,25 +38,6 @@ describe('select.none', () => {
     expect(result).toEqual(expected);
   });
 });
-
-describe('select.nonefree', () => {
-  const initRows = [
-    { id: 10, product: 'apple', company: 'Apple Inc', price: 1.5, stock: 300 },
-    { id: 11, selected:true, product: 'pear', company: 'Pear Inc', price: 3, stock: 1000 },
-    { id: 12, product: 'grape', company: 'Grapesoft', price: 22.1, stock: 18 },
-    { id: 13, selected:false, product: 'banana', company: 'Banana Tech', price: 12, stock: 9 }
-  ];
-  const expected = [
-    { id: 10,  product: 'apple', company: 'Apple Inc', price: 1.5, stock: 300 },
-    { id: 11,  product: 'pear', company: 'Pear Inc', price: 3, stock: 1000 },
-    { id: 12,  product: 'grape', company: 'Grapesoft', price: 22.1, stock: 18 },
-    { id: 13,  product: 'banana', company: 'Banana Tech', price: 12, stock: 9 }
-  ];
-  const result = selectabular.nonefree(initRows);
-  it('returns all rows with no selected field', () => {
-    expect(result).toEqual(expected);
-  });
-});
 describe('select.rows', () => {
   const initRows = [
     { id: 10, product: 'apple', company: 'Apple Inc', price: 1.5, stock: 300 },
@@ -77,9 +50,16 @@ describe('select.rows', () => {
     { id: 12,  product: 'grape', company: 'Grapesoft', price: 22.1, stock: 18 },
     { id: 13,  product: 'banana', company: 'Banana Tech', price: 12, stock: 9 }
   ];
+  const expectedRows = [
+    { id: 10, product: 'apple', company: 'Apple Inc', price: 1.5, stock: 300 },
+    { id: 11,  product: 'pear', company: 'Pear Inc', price: 3, stock: 1000 },
+    { id: 12, selected:true, product: 'grape', company: 'Grapesoft', price: 22.1, stock: 18 },
+    { id: 13, selected: true, product: 'banana', company: 'Banana Tech', price: 12, stock: 9 }
+  ];
   const {rows, selectedRows: result } = selectabular.rows(fil)(initRows);
   it('expect correct filtering of rows', () => {
     expect(result).toEqual(expected);
+    expect(expectedRows).toEqual(rows);
   });
 });
 describe('select.toggle', () => {
@@ -90,10 +70,11 @@ describe('select.toggle', () => {
     { id: 13, product: 'banana', company: 'Banana Tech', price: 12, stock: 9 }
   ];
   const fil = row => row.id < 12
-  //Returns matched ones inverting 'selected'
   const expected = [
     { id: 10, selected: true, product: 'apple', company: 'Apple Inc', price: 1.5, stock: 300 },
-    { id: 11, selected: false, product: 'pear', company: 'Pear Inc', price: 3, stock: 1000 }
+    { id: 11, selected: false, product: 'pear', company: 'Pear Inc', price: 3, stock: 1000 },
+    { id: 12, product: 'grape', company: 'Grapesoft', price: 22.1, stock: 18 },
+    { id: 13, product: 'banana', company: 'Banana Tech', price: 12, stock: 9 }
   ];
   const result = selectabular.toggle(fil)(initRows);
   it('expect correct filering of rows', () => {
